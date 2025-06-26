b851920926bcb498f9c6335aed8bf529129dce12
Srinija Reddy Pendri <nijaredde@gmail.com>
2025-06-26 21:58:42 +0530

    authpage changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import AuthPage from './components/AuthPage';
+import AuthPage from './pages/AuthPage';
 
 
 function App() {
   return (
     <Router>
-      
       <Routes>
         <Route exact path ="/login" element={<AuthPage/>}/>
       </Routes>
